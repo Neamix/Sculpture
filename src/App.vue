920c29c018a26e2d6a7390d3b8fcd69fdeadfa05
AbdalrhmanHussin12 <94906400+AbdalrhmanHussin12@users.noreply.github.com>
2022-01-03 13:55:11 +0200

    remove dublicated header

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,7 @@
 <template>
   <div class="container bg-silver-1000 min-h-screen	min-w-full">
-      <Header/>
       <Navbar/>
-      <header class="w-full min-h-screen flex items-center relative">
-         <div class="p-10 relative z-10">
-            <h2 class="text-white font-bold w-1/2">Sia Career Life</h2>
-            <div class="podcast-stars flex text-white mt-2 mb-2">
-               <i class="ri-star-fill"></i>
-               <i class="ri-star-fill pl-2"></i>
-               <i class="ri-star-fill pl-2"></i>
-               <i class="ri-star-fill pl-2"></i>
-               <i class="ri-star-line pl-2"></i>
-            </div>
-            <p class="text-sm w-1/2 text-white">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
-            <button class="btn relative flex">
-              <i class="ri-headphone-line z-10 pr-2"></i>
-              <p class="z-10">Listen Podcast</p>
-            </button>
-         </div>
-         <div class="overlay w-full h-full "></div>
-      </header>
+      <Header/>
   </div>
 </template>
 
